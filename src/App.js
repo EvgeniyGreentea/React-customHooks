b0707564e68842341629d60bdc8f4b4144d47144
Евгений Храмков <evgeniygreentea@gmail.com>
2021-12-06 00:27:23 +0300

    UseRequest

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react'
-import  List  from './components/List';
+import axios from 'axios';
+import useRequest from './hooks/useRequest'
+
 
 
 function App() {
 
- 
-  return (
-    <div className="App">
-     <List/>
-    </div>
-  );
+  const [todos, loading, error] = useRequest(fetchTodos)
+
+  function fetchTodos() {
+    return axios.get(`https://jsonplaceholder.typicode.com/todos`)
+  }
+
+if(loading) {
+  return <h1>Идет Загрузка</h1>
+}
+
+return (
+  <div className="App">
+    {todos && todos.map(todo =>
+      <div style={{ padding: 30, border: '2px solid black' }}>
+        {todo.id}.{todo.title}
+      </div>)}
+  </div>
+);
 }
 
 export default App;
---
+++ src/hooks/useRequest.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+
+export default function (request) {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(null)
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+        setLoading(true)
+        request()
+            .then(response => setData(response.data))
+            .catch(error => setError(error))
+            .finally(() => setLoading(false))
+
+    }, [])
+    return[data,loading,error]
+}
+
